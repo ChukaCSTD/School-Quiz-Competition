78d53634c2caa33379b45f1f72d6d6d7a29a5f95
Chuka Ozoadibe <chuka.ozoadibe@gmail.com>
2025-09-28 21:26:20 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 // register.js
 
 // Registration deadline (change the new Date to whatever you like)
-const registrationDeadline = new Date('October 29, 2024');
+const registrationDeadline = new Date('October 2, 2024');
 const currentDate = new Date();
 
 // Display deadline message and fine if past deadline
